036a9596d665fbd9e7963a178d837e96c3f37acc
LightKazuto <ilhamsidi62@gmail.com>
2023-11-08 17:49:48 +0700

    fifth commit

---
+++ js/wanita.js
@@ -0,0 +1,40 @@
+window.onload = () => {
+    let button = document.querySelector("#btn");
+ 
+    button.addEventListener("click", calculateBMI);
+};
+
+function calculateBMI() {
+    
+    let weight = parseInt(document
+        .querySelector("#weight").value);
+
+    let height = parseInt(document
+            .querySelector("#height").value);
+    
+    let result = document.querySelector("#result");
+
+
+    if (height === "" || isNaN(height)) 
+        result.innerHTML = "Provide a valid Height!";
+ 
+    else if (weight === "" || isNaN(weight)) 
+        result.innerHTML = "Provide a valid Weight!";
+ 
+
+    else {
+ 
+        let bmi = (weight / ((height * height) 
+                            / 10000)).toFixed(2);
+ 
+        if (bmi < 18.6) result.innerHTML =
+            `Berat Badan Dibawah Rata-rata : <span>${bmi}</span>`;
+ 
+        else if (bmi >= 18.6 && bmi < 24.9) 
+            result.innerHTML = 
+                `Berat Badan Ideal : <span>${bmi}</span>`;
+ 
+        else result.innerHTML =
+            `Berat Badan berlebih (obesitas) : <span>${bmi}</span>`;
+    }
+}
